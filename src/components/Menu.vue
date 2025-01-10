16e787bbe85bb3fc2bac8dcca39b6e4a4d92f919
jcahela <jpacahela@gmail.com>
2025-01-10 13:10:20 -0600

    Add voice recording composable and test in Menu component

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,50 @@
-<script setup lang="ts">
+<script setup>
+import { computed } from 'vue';
+import { useVoiceRecording } from '../composables/useVoiceRecording';
+
+const {
+  startRecording,
+  stopRecording,
+  isRecording,
+  getAudioBlob
+} = useVoiceRecording();
+
+const playbackRecording = async () => {
+  const audioBlob = await getAudioBlob();
+
+  if (audioBlob) {
+    const audio = new Audio(URL.createObjectURL(audioBlob));
+    audio.play();
+  }
+};
+
+const recordingExists = computed(() => {
+  return !!getAudioBlob();
+})
 
 </script>
 
 <template>
   <div class="menu">
+    <button 
+            v-if="!isRecording" 
+            @click="startRecording"
+        >
+            Start Recording
+        </button>
+        <button 
+            v-else 
+            @click="stopRecording"
+        >
+            Stop Recording
+        </button>
+        
+        <button 
+            @click="playbackRecording" 
+            :disabled="!recordingExists"
+        >
+            Play Recording
+        </button>
   </div>
 </template>
 
---
+++ src/composables/useVoiceRecording.js
@@ -0,0 +1,57 @@
+// src/composables/useVoiceRecognition.js
+import { ref } from 'vue'
+
+export function useVoiceRecording() {
+    const isRecording = ref(false)
+    const audioChunks = ref([])
+    let mediaRecorder = null
+    let audioStream = null
+
+    const startRecording = async () => {
+        if (isRecording.value) return
+        isRecording.value = true;
+        try {
+            audioStream = await navigator.mediaDevices.getUserMedia({ 
+                audio: {
+                    sampleRate: 16000,
+                    channelCount: 1
+                }
+            })
+            
+            mediaRecorder = new MediaRecorder(audioStream)
+            audioChunks.value = []
+
+            mediaRecorder.ondataavailable = (event) => {
+                audioChunks.value.push(event.data)
+                console.log('Audio chunk received:', event.data.size, 'bytes')
+            }
+
+            mediaRecorder.start(100) // Collect data in chunks every 100ms
+            isRecording.value = true
+            console.log('Recording started')
+        } catch (err) {
+            console.error('Error starting recording:', err)
+        }
+    }
+
+    const stopRecording = () => {
+        if (mediaRecorder && isRecording.value) {
+            mediaRecorder.stop()
+            audioStream.getTracks().forEach(track => track.stop())
+            isRecording.value = false
+            console.log('Recording stopped')
+        }
+    }
+
+    const getAudioBlob = () => {
+        if (audioChunks.value.length === 0) return null
+        return new Blob(audioChunks.value, { type: 'audio/webm' })
+    }
+
+    return {
+        isRecording,
+        startRecording,
+        stopRecording,
+        getAudioBlob
+    }
+}
